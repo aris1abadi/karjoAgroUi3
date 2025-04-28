0dc9b405d26fb3e4de785513301bef659f89539e
aris1abadi <aris1abadi@gmail.com>
2025-04-28 18:48:50 +0700

    modified:   package-lock.json 	modified:   svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,18 +2,18 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
-		//prerender: {
-		//	default: true
-		//}
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html', // direkomendasikan untuk GitHub Pages
+      precompress: false,
+      strict: true
+    }),
+    //prerender: {
+    //  default: true // <<-- ini boleh untuk SvelteKit 2.16.0
+    //}
+  }
 };
 
 export default config;
